a11d41ff6f1a0b299c38ccc32dfb8f098b7a4402
Apurba <apurba10024@gmail.com>
2019-09-23 14:08:05 +0600

    1.9 Created Databinding instance

--- app/src/main/java/com/example/boardingpass/MainActivity.java
+++ app/src/main/java/com/example/boardingpass/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.boardingpass;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import com.example.boardingpass.databinding.ActivityMainBinding;
+import com.example.boardingpass.utilities.FakeDataUtils;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        BoardingPassInfo fakeBoardingInfo = FakeDataUtils.generateFakeBoardingPassInfo();
     }
 }
